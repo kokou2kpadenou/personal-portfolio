b9b445cf4cedfdc06abbcd3384a336161ff08291
kokou kpadenou <kkokou@gmail.com>
2019-04-30 11:57:13 -0400

    First version

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
+'use strict';
+
+var skillDesc = document.getElementsByClassName('skill__desc');
+var skillScreenDesc = document.querySelector('.skill__screen-desc');
+var navigationCheckbox = document.querySelector('.navigation__checkbox');
+var menuElement = document.getElementsByClassName('navigation__menu__elt');
+var headerName = document.querySelector('.header-name');
+
+
 var swiper = new Swiper('.swiper-container', {
   effect: 'coverflow',
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: 'auto',
+  loop: true,
   coverflowEffect: {
     rotate: 50,
     stretch: 0,
@@ -18,4 +28,39 @@ var swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination',
     clickable: true,
   },
+  on: {
+    init : function() {
+      showSkillDesc(0);
+    },
+    slideChange: function() {
+      if (swiper) {
+        showSkillDesc(swiper.realIndex);
+      }
+    },
+  },
+});
+
+
+function showSkillDesc(index) {
+  skillScreenDesc.innerText = skillDesc[index].innerText;
+}
+
+function menuElementClickHandle() {
+  navigationCheckbox.checked = false;
+}
+
+for (var i = 0; i < menuElement.length; i++) {
+  menuElement[i].addEventListener('click', menuElementClickHandle);
+}
+
+window.addEventListener('scroll', function(e) {
+  if ((document.body.scrollTop > 130) || (document.documentElement.scrollTop > 130)) {
+    // Name visible in the header
+    headerName.classList.add('show');
+  } else {
+    // Header without name
+    if (headerName.classList.contains('show')) {
+      headerName.classList.remove('show');
+    }
+  }
 });
