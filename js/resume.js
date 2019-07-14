168f954ec8a11eff6c400f9dae2230bf872ce5ca
kokou kpadenou <kkokou@gmail.com>
2019-07-14 13:29:36 -0400

    Resume final 1

--- js/resume.js
+++ js/resume.js
@@ -1,6 +1,6 @@
 var btnPrint = document.getElementById('btn-print');
 var btnCanvas = document.getElementById('btn-canvas');
-
+var titleBar = document.getElementsByClassName('title-bar-click');
 
 btnPrint.addEventListener('click', function(e) {
   e.preventDefault();
@@ -11,3 +11,13 @@ btnCanvas.addEventListener('click', function(e) {
   e.preventDefault();
   document.querySelector('html').classList.toggle('bg-darkness');
 });
+
+function toggleExpand(e) {
+  var expand = e.currentTarget.previousElementSibling;
+  e.preventDefault();
+  expand.checked = !expand.checked;
+}
+
+for (var i = 0; i < titleBar.length; i++) {
+  titleBar[i].addEventListener('click', toggleExpand);
+}
